a1f690c840541f32923c7af45cdd1a8d461be8c1
StephanBroeker <119671047+StephanBroeker@users.noreply.github.com>
2023-04-14 10:39:44 +0200

    Missing Name in __init__ file

--- abellipsoids2d/__init__.py
+++ abellipsoids2d/__init__.py
@@ -9,6 +9,6 @@ from ._reconstruct import __all__ as reconstruct_all
 __all__ = reconstruct_all
 
 __author__ = "Stephan Bröker, Raphael Wittkowski"
-__copyright__ = "Copyright (C) 2022 Stephan Bröker, Raphael Wittkowski"
+__copyright__ = "Copyright (C) 2022 Stephan Bröker, Michael te Vrugt, Raphael Wittkowski"
 __license__ = "MIT"
 __version__ = "1.0"
